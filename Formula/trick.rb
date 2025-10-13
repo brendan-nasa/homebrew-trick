7d64991571df7e7d4464d5b88979a13ef8861e3d
Brendan Fattig <brendan.fattig@nasa.gov>
2025-10-13 16:02:30 -0500

    Add `env :std` to keep env variables

--- Formula/trick.rb
+++ Formula/trick.rb
@@ -4,6 +4,8 @@ class Trick < Formula
   license "NASA-1.3"
   head "https://github.com/nasa/trick.git", branch: "master"
 
+  env :std
+
   depends_on "llvm" => :build
   depends_on "maven" => :build
   depends_on "swig" => :build
