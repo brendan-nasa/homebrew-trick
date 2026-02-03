c2f97f67c20c4e4b1964fa9b8da2e017ac478e2f
brendan <brendan.fattig@nasa.gov>
2026-02-03 12:39:48 -0600

    trick 25.0.2

--- Formula/trick.rb
+++ Formula/trick.rb
@@ -1,56 +1,69 @@
 class Trick < Formula
-  desc "Powerful simulation development framework"
-  homepage "https://nasa.github.io/trick"
+  desc "Simulation development framework"
+  homepage "https://github.com/nasa/trick"
+  url "https://github.com/nasa/trick/archive/refs/tags/25.0.2.tar.gz"
+  sha256 "0e51ee68d7239423a18b144dea9e0560125c523a0e2d8a914933ace1bba39dd3"
   license "NASA-1.3"
   head "https://github.com/nasa/trick.git", branch: "master"
 
-  depends_on "llvm" => :build
-  depends_on "maven" => :build
-  depends_on "swig" => :build
-  depends_on "googletest" => :test
-  depends_on "java"
+  depends_on "pkgconf" => :build
+  depends_on arch: :arm64
+  depends_on "gsl"
+  depends_on "hdf5"
+  depends_on "libaec"
+  depends_on "llvm"
   depends_on :macos
+  depends_on "maven"
+  depends_on "openjdk"
+  depends_on "openmotif"
   depends_on "perl"
-  depends_on "python3"
+  depends_on "python"
+  depends_on "swig"
   depends_on "udunits"
-  depends_on "gsl" => :recommended
-  depends_on "hdf5" => :recommended
-  depends_on "openmotif" => :recommended
 
   def install
-    # Set build environment
-    ENV["TRICK_HOME"] = buildpath
-
-    # Configure & build
-    system "./configure", "--prefix=#{prefix}"
+    system "./configure",
+        "--with-udunits=#{Formula["udunits"].opt_prefix}",
+        "--with-hdf5=#{Formula["hdf5"].opt_prefix}",
+        "--with-gsl=#{Formula["gsl"].opt_prefix}"
     system "make", "-j#{ENV.make_jobs}"
-    system "make", "install"
 
-    # Environment wrapper script
-    (share/"trick_env.sh").write <<~EOS
-      #!/bin/bash
-      export TRICK_HOME=#{opt_prefix}
-      export PATH=#{opt_bin}:$PATH
-    EOS
-    chmod 0755, share/"trick_env.sh"
+    bin.install Dir["bin/*"] if File.directory?("bin")
+    lib.install Dir["lib/*"] if File.directory?("lib")
+    include.install Dir["include/*"] if File.directory?("include")
+    share.install Dir["share/*"] if File.directory?("share")
+    libexec.install Dir["libexec/*"] if File.directory?("libexec")
+
+    # Install trick_source/er7_utils
+    (prefix/"trick_source").install "trick_source/er7_utils"
+
+    # Clean up build artifacts
+    rm_r(Dir[prefix/"trick_source/**/object_*"])
+    rm_r(Dir[prefix/"trick_source/**/io_src"])
+
+    # Fix hardcoded shim compiler paths in installed config file
+    inreplace pkgshare/"makefiles/config_user.mk" do |s|
+      s.gsub! "super/clang++", "clang++"
+      s.gsub! "super/clang", "clang"
+      s.gsub! "super/ld", "ld"
+      s.gsub! %r{#{HOMEBREW_SHIMS_PATH}/[^/]+/}o, ""
+    end
+    # Fix HDF5 library paths to include libaec for libsz
+    inreplace pkgshare/"makefiles/Makefile.common",
+                "HDF5_LIB := -L$(HDF5)/lib -lhdf5_hl -lhdf5 -lsz",
+                "HDF5_LIB := -L$(HDF5)/lib -L#{Formula["libaec"].opt_lib} -lhdf5_hl -lhdf5 -lsz"
   end
 
   def caveats
     <<~EOS
-      Trick has been installed to:
-        #{opt_prefix}
-
-      To use Trick, set the environment variables:
+      To use Trick, set the TRICK_HOME environment variable:
         export TRICK_HOME=#{opt_prefix}
-        export PATH=#{opt_bin}:$PATH
 
-      Or source the provided script:
-        source #{opt_share}/trick_env.sh
+      Add this to your shell profile (~/.zshrc or ~/.bash_profile)
     EOS
   end
 
   test do
-    # Test that trick-CP binary exists and runs
     assert_path_exists bin/"trick-CP"
     system bin/"trick-CP", "--help"
   end
